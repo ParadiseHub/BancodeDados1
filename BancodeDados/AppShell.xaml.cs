24d14fef985882f03d7b582ec846ab86d3d5dfdc
Usuario <Usuario@CPD03-M10>
2024-02-26 11:17:09 -0300

    Adicionar arquivos de projeto.

---
+++ BancodeDados/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BancodeDados
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BancodeDados/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BancodeDados
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
